1c5a025937b804011d04694e22575c8535e12cb3
zhengwei23 <zhengwei23@baidu.com>
2023-03-08 10:58:01 +0800

    lint

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    mocha: true,
+  },
+  extends: ["eslint:recommended", "prettier"],
+  overrides: [],
+  plugins: ["prettier"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "no-prototype-builtins": 0,
+    "prettier/prettier": "error",
+  },
+};
